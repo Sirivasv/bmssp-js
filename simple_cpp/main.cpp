b9176ec310b0daf9a57bafe09ff4d718c1ab679f
Saul Ivan Rivas Vega <saul.ivan.rivas.vega@gmail.com>
2025-08-18 20:41:28 +0200

    Add .gitignore and base hello world code

---
+++ simple_cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(0); ios_base::sync_with_stdio(0);
+    int a, b;
+    cin >> a >> b;
+    cout << "Hello, Tsinghua SSSP!" << endl;
+    cout << "The sum of " << a << " and " << b << " is " << a + b << "." << endl;
+    return 0;
+}
