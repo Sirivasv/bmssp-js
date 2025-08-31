c98a38b917c32632a642145c6712920fe07333fa
Saul Ivan Rivas Vega <saul.ivan.rivas.vega@gmail.com>
2025-08-31 13:04:57 +0200

    feat: modify test

--- test/main.test.js
+++ test/main.test.js
@@ -3,7 +3,7 @@ import { printMessage } from "../src/bmssp.mjs";
 
 describe("printMessage", () => {
   test("sends a message and gets the message back", () => {
-    let testValue = "Hello, bmssp! Testing..";
+    let testValue = "Hello, bmssp!! Testing..";
     expect(printMessage(testValue)).toBe(testValue);
   });
 });
